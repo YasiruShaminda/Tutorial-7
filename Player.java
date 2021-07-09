9995cbf1902b107f4748bf546a0d0ade5d0b7654
duniyash <80444721+duniyash@users.noreply.github.com>
2021-07-09 14:26:11 +0530

    edit 1

---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    //instance variables
+    private String name;
+    private int age;
+    private String playerType;
+    private int runs;
+    private int wickets;
+
+   
+
+}
